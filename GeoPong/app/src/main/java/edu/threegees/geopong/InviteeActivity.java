2059d05026083b4cda17c8a68e9a9f0f4d676783
Benjamin Boyle <bboyle2@upei.ca>
2018-11-16 17:51:39 -0400

    Added some preliminaries for Google Play Services integration

---
+++ GeoPong/app/src/main/java/edu/threegees/geopong/InviteeActivity.java
@@ -0,0 +1,26 @@
+//FOR LATER
+
+package edu.threegees.geopong;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InviteeActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invitee);
+
+        /* For later if App Links are implemented
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+        */
+    }
+}
--- GeoPong/app/src/main/java/edu/threegees/geopong/MainMenuActivity.java
+++ GeoPong/app/src/main/java/edu/threegees/geopong/MainMenuActivity.java
@@ -1,5 +1,8 @@
 package edu.threegees.geopong;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +14,12 @@ public class MainMenuActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //force app to run in portrait for consistent experience
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
     }
 }
