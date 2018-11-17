f26e7562529817eaaae6b192a104aaf865e49333
Benjamin Boyle <bboyle2@upei.ca>
2018-11-16 21:33:23 -0400

    Corrected fragment imports

--- GeoPong/app/src/main/java/edu/threegees/geopong/EndGameFragment.java
+++ GeoPong/app/src/main/java/edu/threegees/geopong/EndGameFragment.java
@@ -2,16 +2,12 @@ package edu.threegees.geopong;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class EndGameFragment extends Fragment
+public class EndGameFragment extends android.support.v4.app.Fragment
 {
 
 
--- GeoPong/app/src/main/java/edu/threegees/geopong/PongGameFragment.java
+++ GeoPong/app/src/main/java/edu/threegees/geopong/PongGameFragment.java
@@ -2,16 +2,12 @@ package edu.threegees.geopong;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class PongGameFragment extends Fragment
+public class PongGameFragment extends android.support.v4.app.Fragment
 {
 
 
