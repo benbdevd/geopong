98fca86bf0a3a1f720d93bf839ff9d865937de58
Wildman644 <wildmancaitlin@gmail.com>
2018-11-27 13:17:14 -0400

    Added mp3
    asdfgjm

--- GeoPong/app/src/main/java/edu/threegees/geopong/MainMenuActivity.java
+++ GeoPong/app/src/main/java/edu/threegees/geopong/MainMenuActivity.java
@@ -2,6 +2,7 @@ package edu.threegees.geopong;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +10,14 @@ import android.view.View;
 public class MainMenuActivity extends AppCompatActivity
 {
     private int mGameMode;
+    protected MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.geopong);
 
         //force app to run in portrait for consistent experience on phone
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -49,5 +52,22 @@ public class MainMenuActivity extends AppCompatActivity
         intent.putExtra("game_mode", mGameMode);
 
         startActivity(intent);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        //begins background music
+        mMediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mMediaPlayer.stop();
+        mMediaPlayer.release();
     }
 }
